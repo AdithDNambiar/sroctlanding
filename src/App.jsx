2c45b999881f375517369b5e7d164f597bbe698b
Adith Nambiar <adiadithdnambiar@gmail.com>
2026-06-20 14:11:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="landing">
+      <div className="hero">
+        <h1>SROCT</h1>
+        <p>BUILD • CONNECT • GROW</p>
+      </div>
+    </div>
+  );
+}
